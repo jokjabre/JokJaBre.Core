c7cff8797c4abdeadbc31873ee37f02192da53f6
jok ja bre <jokjabre@outlook.com>
2019-07-10 16:51:53 +0200

    changed JsonConfig section name

--- JokJaBre.Core.Auth/Identity/JokJaBreIdentity.cs
+++ JokJaBre.Core.Auth/Identity/JokJaBreIdentity.cs
@@ -15,7 +15,7 @@ namespace JokJaBre.Core.Auth.Identity
         public static string HashPassword(this IJokJaBreIdentityModel user, IConfiguration configuration, int iterationCount = 10000)
         {
             // generate a 128-bit salt using a secure PRNG
-            byte[] salt = Encoding.Unicode.GetBytes(configuration["Jwt:Salt"]);
+            byte[] salt = Encoding.Unicode.GetBytes(configuration["JokJaBre:Salt"]);
 
             // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
             string hashed = Convert.ToBase64String(
@@ -39,14 +39,14 @@ namespace JokJaBre.Core.Auth.Identity
                     new Claim(JwtRegisteredClaimNames.Sub, user.Username)
                 };
             var signinKey = new SymmetricSecurityKey(
-              Encoding.UTF8.GetBytes(configuration["Jwt:SigningKey"]));
+              Encoding.UTF8.GetBytes(configuration["JokJaBre:Auth:SigningKey"]));
 
-            int expiryInMinutes = Convert.ToInt32(configuration["Jwt:ExpiresIn"]);
+            int expiryInMinutes = Convert.ToInt32(configuration["JokJaBre:Auth:ExpiresIn"]);
 
             var token = new JwtSecurityToken(
               claims: claim,
-              issuer: configuration["Jwt:Site"],
-              audience: configuration["Jwt:Site"],
+              issuer: configuration["JokJaBre:Auth:Site"],
+              audience: configuration["JokJaBre:Auth:Site"],
               expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
               signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
             );
