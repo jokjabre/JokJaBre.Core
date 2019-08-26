b81c915bba57b6bb4149718d6af1a3857ff6d276
Marko Milic <jokjabre@gmail.com>
2019-08-26 13:37:00 +0200

    Implemented SoftDelete

---
+++ JokJaBre.Core.API/Context/JokJaBreContext.cs
@@ -0,0 +1,41 @@
+﻿using JokJaBre.Core.Objects.Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JokJaBre.Core.API.Context
+{
+    public class JokJaBreContext : DbContext
+    {
+        protected bool ImplementsSoftDelete { get; set; } = true;
+
+        private void DoSoftDelete()
+        {
+            if (!ImplementsSoftDelete) return;
+
+            ChangeTracker.DetectChanges();
+            var deleted = ChangeTracker.Entries<IJokJaBreSoftDeletable>().Where(x => x.State == EntityState.Deleted);
+            foreach (var entity in deleted)
+            {
+                entity.State = EntityState.Modified;
+                entity.Entity.DateRemoved = DateTime.Now;
+            }
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            DoSoftDelete();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            DoSoftDelete();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
---
+++ JokJaBre.Core.Objects/Objects/IJokJaBreSoftDeletable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JokJaBre.Core.Objects.Objects
+{
+    public interface IJokJaBreSoftDeletable
+    {
+        DateTime DateRemoved { get; set; }
+    }
+}
