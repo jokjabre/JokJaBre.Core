151fd42dfdab5b1e2dbf6a661e9f6b64cbd3e92e
Marko Milic <jokjabre@gmail.com>
2019-09-11 16:47:47 +0200

    Removed collection mapping

--- JokJaBre.Core.API/Controller/JokJaBreController.cs
+++ JokJaBre.Core.API/Controller/JokJaBreController.cs
@@ -7,14 +7,23 @@ using System.Linq;
 
 namespace JokJaBre.Core.API
 {
-    public abstract class JokJaBreController<TModel> : JokJaBreControllerBase
+    public abstract class JokJaBreController<TModel> : JokJaBreController<TModel, IJokJaBreService<TModel>>
         where TModel : class, IJokJaBreModel
     {
-        protected IJokJaBreService<TModel> m_service;
+        public JokJaBreController(IJokJaBreService<TModel> service) : base(service)
+        {
+        }
+    }
+
+    public abstract class JokJaBreController<TModel, TService> : JokJaBreControllerBase
+        where TModel : class, IJokJaBreModel
+        where TService: class, IJokJaBreService<TModel>
+    {
+        protected TService m_service;
         protected Func<DbSet<TModel>, IQueryable<TModel>> m_defaultIncludes;
         public JokJaBreController(IJokJaBreService<TModel> service)
         {
-            m_service = service;
+            m_service = service as TService;
 
             //no includes
             m_defaultIncludes = i => i;
--- JokJaBre.Core.API/Extensions/JokJaBreConversionExtensions.cs
+++ JokJaBre.Core.API/Extensions/JokJaBreConversionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using JokJaBre.Core.API.Attributes;
-using JokJaBre.Core.Objects;
+﻿using JokJaBre.Core.Objects;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,7 +24,7 @@ namespace JokJaBre.Core.Extensions
             where TModel : IJokJaBreModel
             where TResponse : IJokJaBreResponse
         {
-            return models.Select(model => model.ToResponse<TResponse>());
+            return models?.Select(model => model.ToResponse<TResponse>());
         }
 
         public static TModel ToModel<TRequest, TModel>(this TRequest request)
@@ -41,44 +40,12 @@ namespace JokJaBre.Core.Extensions
             return model;
         }
 
-        public static IEnumerable<TModel> ToModel<TRequest, TModel>(this IEnumerable<TRequest> models)
+        public static IEnumerable<TModel> ToModel<TRequest, TModel>(this IEnumerable<TRequest> requests)
             where TRequest : IJokJaBreRequest
             where TModel : IJokJaBreModel
         {
-            return models.Select(model => model.ToModel<TRequest, TModel>());
+            return requests?.Select(request => request.ToModel<TRequest, TModel>());
         }
-
-
-        //public static void CopyTo<TSource, TDestination>(this TSource source, TDestination destination)
-        //    where TSource : IJokJaBreObject
-        //    where TDestination : IJokJaBreObject
-        //{
-        //    var sourceProps = source.GetType().GetProperties();
-        //    var destType = destination.GetType();
-        //    foreach (var prop in sourceProps)
-        //    {
-        //        var destProp = destType.GetProperty(prop.Name);
-        //        if (destProp == null) continue;
-
-        //        //collection mapping
-        //        var customAttr = destProp.GetCustomAttributes(true)
-        //            .FirstOrDefault(a => a is MapAsCollectionAttribute) as MapAsCollectionAttribute;
-        //        if (customAttr != null)
-        //        {
-        //            var collection = prop.GetValue(source) as IEnumerable;
-        //            var genericArg = destProp.PropertyType.GetGenericArguments().FirstOrDefault();
-        //            if (genericArg == null) continue;
-
-        //            destProp?.SetValue(destination, ConvertCollection(collection, genericArg).ToList());
-
-        //            continue;
-        //        }
-
-        //        //property mapping
-        //        if (!prop.PropertyType.IsAssignableFrom(destProp.PropertyType)) continue;
-        //        destProp?.SetValue(destination, prop.GetValue(source));
-        //    }
-        //}
 
         public static void CopyTo(this object source, object destination)
         {
@@ -89,33 +56,12 @@ namespace JokJaBre.Core.Extensions
                 var destProp = destType.GetProperty(prop.Name);
                 if (destProp == null) continue;
 
-                //collection mapping
-                var customAttr = destProp.GetCustomAttributes(true).OfType<MapAsCollectionAttribute>().FirstOrDefault();
-                if (customAttr != null)
-                {
-                    var collection = prop.GetValue(source) as IEnumerable;
-                    var genericArg = destProp.PropertyType.GetGenericArguments().FirstOrDefault();
-                    if (genericArg == null) continue;
+                //conditions
+                bool assignable = prop.PropertyType.IsAssignableFrom(destProp.PropertyType);
+                bool sameGenArgs = prop.PropertyType.GetGenericArguments().SequenceEqual(destProp.PropertyType.GetGenericArguments());
+                if (!assignable || !sameGenArgs) continue;
 
-                    destProp?.SetValue(destination, ConvertCollection(collection, genericArg).AsQueryable());
-
-                    continue;
-                }
-
-                //property mapping
-                if (!prop.PropertyType.IsAssignableFrom(destProp.PropertyType)) continue;
                 destProp?.SetValue(destination, prop.GetValue(source));
-            }
-        }
-
-
-        private static IEnumerable ConvertCollection(IEnumerable source, Type destType)
-        {
-            foreach(var model in source)
-            {
-                var dest = Activator.CreateInstance(destType);
-                model.CopyTo(dest);
-                yield return dest;
             }
         }
     }
